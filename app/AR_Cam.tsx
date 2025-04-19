e605cdb61be86be4ea4e8df10233d8a85a533f94
Joseph Ryan Chico <chico.213413@caloocan.sti.edu.ph>
2025-04-19 20:26:45 +0800

    fixed

--- app/AR_Cam.tsx
+++ app/AR_Cam.tsx
@@ -1,25 +1,10 @@
-/* eslint-disable */
-import React from 'react';
-import { ViroARScene, ViroARSceneNavigator, ViroAmbientLight, Viro3DObject } from '@viro-community/react-viro';
-import { View, Text} from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
 
-const AR_Cam = () => {
-
-
-    return (
-        <ViroARScene>
-            <ViroAmbientLight color="#ffffff" />
-            <Viro3DObject
-                source={require('../assets/font_occlusion.jpeg')}
-
-                scale={[0.008, 0.008, 0.008]}
-                rotation={[-170, 0, 0]}
-                type="OBJ"
-            />
-
-        </ViroARScene>
-       
-    )
+export default function AR_Cam() {
+  return (
+    <View>
+      <Text>AR_Cam</Text>
+    </View>
+  )
 }
-
-export default AR_Cam
